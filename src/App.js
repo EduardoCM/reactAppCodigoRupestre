a1adbdd78599d31dc357edf72d799fd4aa9d3314
Eduardo Castillo <ecastillo@anzen.com.mx>
2020-09-03 11:58:44 -0500

    Se actualiza logo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
+import codigorupestre from './codigo.png';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          I am a Cloud Native Developer
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <h1>  I am a Cloud Native Developer </h1>
+        <h2> Codigo Rupestre - Space Native </h2>
+
+          <img src={codigorupestre} className="App-logo" alt="logo" />
+
+
+
+
       </header>
     </div>
   );
